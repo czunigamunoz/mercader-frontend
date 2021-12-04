dfba89dedaaa39e8f4d5e35c26b83d4875a22444
czunigamunoz <62579058+czunigamunoz@users.noreply.github.com>
2021-12-03 19:17:47 -0500

    v0.3 completed reto2

--- js/admin-products.js
+++ js/admin-products.js
@@ -5,11 +5,11 @@ let ID_PRODUCT = null;
 // USER INFORMATION ==========================
 // const idProduct = document.getElementById("id");
 const brandProduct = document.getElementById("brand");
-const categoruProduct = document.getElementById("category");
+const categoryProduct = document.getElementById("category");
 const nameProduct = document.getElementById("name");
 const descriptionProduct = document.getElementById("description");
 const priceProduct = document.getElementById("price");
-const avaliabilityProduct = document.getElementById("avaliability");
+const availabilityProduct = document.getElementById("availability");
 const quantityProduct = document.getElementById("quantity");
 const photoProduct = document.getElementById("photography");
 
@@ -25,7 +25,7 @@ const btnSave = document.getElementById("btnSave");
  * Function to active modal and set default value
  */
 btnAdd.addEventListener("click", () => {
-    categoruProduct.value = "Gadgets";
+    categoryProduct.value = "Gadgets";
     modal.classList.add("modal--active")
 });
 
@@ -48,11 +48,11 @@ btnSave.addEventListener("click", () => saveProduct());
  */
 const clearFields = () => {
     brandProduct.value = "";
-    categoruProduct.value = "";
+    categoryProduct.value = "";
     nameProduct.value = "";
     descriptionProduct.value = "";
     priceProduct.value = "";
-    avaliabilityProduct.value = "";
+    availabilityProduct.value = "";
     quantityProduct.value = "";
     photoProduct.value = "";
 }
@@ -62,17 +62,16 @@ const clearFields = () => {
  * @returns Product object
  */
 const getFieldsInfo = () => {
-    const product = {
+    return {
         brand: brandProduct.value,
-        category: categoruProduct.value,
+        category: categoryProduct.value,
         name: nameProduct.value,
         description: descriptionProduct.value,
         price: parseInt(priceProduct.value),
-        avaliability: avaliabilityProduct.value,
+        availability: availabilityProduct.value,
         quantity: parseInt(quantityProduct.value),
         photography: photoProduct.value
     }
-    return product;
 }
 
 /**
@@ -81,60 +80,59 @@ const getFieldsInfo = () => {
  */
 const setFieldsInfo = (product) => {
     brandProduct.value = product.brand;
-    categoruProduct.value = product.category;
+    categoryProduct.value = product.category;
     nameProduct.value = product.name;
     descriptionProduct.value = product.description;
     priceProduct.value = parseInt(product.price);
-    avaliabilityProduct.value = product.availability;
+    availabilityProduct.value = product.availability;
     quantityProduct.value = parseInt(product.quantity);
     photoProduct.value = product.photography;
+}
+
+const validate = (brand, category, name, description, price, availability, quantity, photography) => {
+    if (brand.length === 0 || category.length === 0 || name.length === 0 
+        || description.length === 0 || price.length === 0 || availability.length === 0 
+        || quantity.length === 0 || photography === 0)  {
+        swalHandler("!Error", "error", "All fields are required", true, "#DC143C");
+        return false;
+    }
+    if (description.length > 80) {
+        swalHandler("!Error", "error", "Description must not exceed 80 characters", true, "#DC143C");
+        return false;
+    }
+    if (typeof price !== "number") {
+        swalHandler("!Error", "error", "Price must be a numerical", true, "#DC143C");
+        return false;
+    }
+    if (typeof quantity !== "number") {
+        swalHandler("!Error", "error", "Quantity must be a numerical", true, "#DC143C");
+        return false;
+    }
+    if (!(/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi.test(photography))){
+        swalHandler("!Error", "error", "Photography must be a URL", true, "#DC143C");
+        return false;
+    }
+    return true;
 }
 
 /**
  * Save or Update product to database
  */
 const saveProduct = async () => {
-    const { brand, category, name, description, price, avaliability, quantity, photography } = getFieldsInfo();
+    const { brand, category, name, description, price, availability, quantity, photography } = getFieldsInfo();
 
-    if (brand.length === 0 || category.length === 0 || name.length === 0 
-        || description.length === 0 || price.length === 0 || avaliability.length === 0 
-        || quantity.length == 0 || photography === 0)  {
-        swalHandler("!Error", "error", "All fields are required", true, "#DC143C");
+    const areValid = validate(brand, category, name, description, price, availability, quantity, photography);
+    if (!areValid) {
         return;
     }
-    if (description.length > 80) {
-        swalHandler("!Error", "error", "Description must not exceed 80 characters", true, "#DC143C");
-        return;
-    }
-    if (typeof price !== "number") {
-        swalHandler("!Error", "error", "Price must be a numerical", true, "#DC143C");
-        return;
-    }
-    if (typeof quantity !== "number") {
-        swalHandler("!Error", "error", "Quantity must be a numerical", true, "#DC143C");
-        return;
-    }
-    if (!(/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi.test(photography))){
-        swalHandler("!Error", "error", "Photography must be a URL", true, "#DC143C");
-        return;
-    }
-    
-    let id;
-    if (!!ID_PRODUCT){
-        id = ID_PRODUCT;      
-    } else {        
-        const products = await ajaxHandler.connectGet(`${URL}/all`);
-        id = products.length > 0 ? products.at(-1).id+1 : 1;
-    }
-
     const data = {
-        id,
+        id: !!ID_PRODUCT ? ID_PRODUCT: null,
         brand,
         category,
         name,
         description,
         price,
-        avaliability,
+        availability,
         quantity,
         photography
     }
@@ -147,7 +145,7 @@ const saveProduct = async () => {
     ID_PRODUCT = null;
     swalHandler("", "success", message, false, "", 1500);
     clearFields();
-    getProducts();
+    await getProducts();
     modal.classList.remove("modal--active");
 }
 
@@ -172,8 +170,8 @@ const deleteProduct = async (id) => {
         return;
     }
     await ajaxHandler.connectDelete(`${URL}/${id}`);
-    getProducts();
-    swalHandler("", "success", "Prodcut eliminated successfully", false, "", 1500);
+    swalHandler("", "success", "Product eliminated successfully", false, "", 1500);
+    await getProducts();
 }
 
 /**
@@ -181,33 +179,31 @@ const deleteProduct = async (id) => {
  */
 const getProducts = async () => {
     const products = await ajaxHandler.connectGet(`${URL}/all`);
-    if (products?.length === 0 || products === null) {
-        swalHandler("No products in database", "warning", "", false, "#3085d6", 2000);
-        return;
+    if (products?.length > 0 || products !== null) {
+        const table = document.getElementById("tableContent");
+        table.innerHTML = "";
+        products.forEach(product => {
+            table.innerHTML += `
+                <tr>
+                    <td data-label="Id">${product.id}</td>
+                    <td data-label="Brand">${product.brand}</td>
+                    <td data-label="Category">${product.category}</td>
+                    <td data-label="Name">${product.name}</td>
+                    <td data-label="Description">${product.description}</td>
+                    <td data-label="Price">${product.price}</td>
+                    <td data-label="Availability">${product.availability}</td>
+                    <td data-label="Quantity">${product.quantity}</td>
+                    <td data-label="Photography"><img src="${product.photography}" alt="${product.brand}"></td>
+                    <td data-label="Edit">
+                        <span role="button" class="material-icons-sharp warning" onclick="updateProduct(${product.id})">edit</span>
+                    </td>
+                    <td data-label="Delete">                                        
+                        <span role="button" class="material-icons-sharp danger" onclick="deleteProduct(${product.id})">delete</span>
+                    </td>
+                </tr>
+                `;
+        });        
     }
-    const table = document.getElementById("tableContent");
-    table.innerHTML = "";
-    products.forEach(product => {
-        table.innerHTML += `
-            <tr>
-                <td data-label="Id">${product.id}</td>
-                <td data-label="Brand">${product.brand}</td>
-                <td data-label="Category">${product.category}</td>
-                <td data-label="Name">${product.name}</td>
-                <td data-label="Description">${product.description}</td>
-                <td data-label="Price">${product.price}</td>
-                <td data-label="Avaliability">${product.availability}</td>
-                <td data-label="Quantity">${product.quantity}</td>
-                <td data-label="Photography"><img src="${product.photography}" alt="${product.brand}"></td>
-                <td data-label="Edit">
-                    <span role="button" class="material-icons-sharp warning" onclick="updateProduct(${product.id})">edit</span>
-                </td>
-                <td data-label="Delete">                                        
-                    <span role="button" class="material-icons-sharp danger" onclick="deleteProduct(${product.id})">delete</span>
-                </td>
-            </tr>
-            `;
-    });
 }
 
-window.onload = getProducts();
+window.onload = async () => { await getProducts()};
